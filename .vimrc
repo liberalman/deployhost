637aa771add62b2dee6c40acac990837873bf4f4
liberalman <zscchina@163.com>
2017-09-19 11:07:05 +0800

    1

--- .vimrc
+++ .vimrc
@@ -46,17 +46,18 @@ filetype plugin indent on    " required
 " " Put your non-Plugin stuff after this line
 
 " ================ setting ycm start ================
-let g:ycm_global_ycm_extra_conf = ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
-let g:ycm_seed_identifiers_with_syntax=1
-let g:ycm_confirm_extra_conf=0
-let g:ycm_cache_omnifunc=0
-let g:ycm_min_num_of_chars_for_completion=2
-let g:ycm_complete_in_comments=1
-let g:ycm_complete_in_strings=1
+"let g:ycm_global_ycm_extra_conf = ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
+"let g:ycm_seed_identifiers_with_syntax=1
+"let g:ycm_confirm_extra_conf=0
+"let g:ycm_cache_omnifunc=0
+"let g:ycm_min_num_of_chars_for_completion=2
+"let g:ycm_complete_in_comments=1
+"let g:ycm_complete_in_strings=1
 " ================ setting ycm end ================
 
 
-
-set ts=4
-set expandtab
-
+set nu " set line number
+set cindent " set indent of c style
+set ts=4 " set tap of 4 spaces
+set expandtab " 
+"set autoindent
--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ softwares=("git" "cmake" "python-devel" )
 for soft in ${softwares[*]}
 do
     echo $soft
-    exist=`rpm -qa $soft`
-    if [ ! -n $exist ]; then
-        # $exist is NULL
+    exist=`rpm -qa ${soft}`
+    echo $exist
+    if [ "" = "$exist" ]; then
+        # $exist is empty
         yum install $soft -y
     fi
 done
@@ -28,14 +29,14 @@ function vim()
         git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
         echo "clone Vundle success"
     fi
-    if [ ! -d ~/.vim/bundle/YouCompleteMe ]; then
-        git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-        pushd ~/.vim/bundle/YouCompleteMe
-            git submodule update --init --recursive
-            ./install.py --clang-completer
-        popd
-        echo "clone YouCompleteMe success"
-    fi
+    #if [ ! -d ~/.vim/bundle/YouCompleteMe ]; then
+    #    git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+    #    pushd ~/.vim/bundle/YouCompleteMe
+    #        git submodule update --init --recursive
+    #        ./install.py --clang-completer
+    #    popd
+    #    echo "clone YouCompleteMe success"
+    #fi
     if [ ! -f ~/.vimrc ]; then
         # file not exist
         ln -s $DEPLOYHOST/.vimrc ~/.vimrc
